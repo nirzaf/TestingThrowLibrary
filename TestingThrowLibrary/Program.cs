6a14c1e3f0871c1de4e97875e4772fc9cea39a8c
Mohamed Fazrin <mfmfazrin1986@gmail.com>
2023-01-30 12:48:02 +0300

    Initial commit

---
+++ TestingThrowLibrary/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Throw;
+
+using static System.Console;
+
+WriteLine("Hello, World!");
+
+try
+{
+    string? data = "Hello, World!";
+    data.ThrowIfNull("data is null");
+    // var input = ReadLine();
+    // input!.Trim().Throw().IfNullOrEmpty(p=>p, "Input is null or empty");
+    WriteLine($"You entered: {data}");
+    ReadLine();
+}
+catch (Exception exception)
+{
+    WriteLine(exception);
+    ReadLine();
+}
+
